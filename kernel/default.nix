6d9ae9e808e84c5e7609202faa9d21b3b0997c0d
uiop <uiop@wasdhjkl.xyz>
2025-09-28 16:00:51 -0400

    feat(kernel): export KERNEL_SRC and KERNEL_BUILD

--- kernel/default.nix
+++ kernel/default.nix
@@ -1,8 +1,9 @@
 { pkgs, self, system }:
 let
   name = ''\033[33mkernel-dev\033[39m'';
+  linux = self.packages.${system}.linux;
 in pkgs.mkShell {
-  buildInputs = with pkgs; [ self.packages.${system}.linux ];
+  buildInputs = with pkgs; [ linux ];
   shellHook = ''
     if [[ -n "$NIX_DEVELOP_STACK" ]]; then
       export NIX_DEVELOP_STACK="$NIX_DEVELOP_STACK|${name}"
@@ -10,5 +11,8 @@ in pkgs.mkShell {
       export NIX_DEVELOP_STACK="${name}"
     fi
     export PS1="\n\033[1m[$NIX_DEVELOP_STACK]\033[0m $PS1"
+
+    export KERNEL_SRC=${linux}/src
+    export KERNEL_BUILD=${linux}/build
   '';
 }
