bd45c2abcf628295bd6fe2035678f90a2d2b528e
uiop <uiop@wasdhjkl.xyz>
2025-09-28 16:39:51 -0400

    refactor(kernel): move scripts and pkg definition

--- flake.nix
+++ flake.nix
@@ -13,40 +13,19 @@
       packages = eachSystem (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          kernelStuff = import ./kernel { inherit pkgs self system; };
         in {
-          linux = pkgs.stdenv.mkDerivation {
-            pname = "linux";
-            version = "6.12.49";
-            src = pkgs.fetchurl {
-              url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.12.49.tar.xz";
-              hash = "sha256-I0Yh4UbazOIkEElVXVUOT3pr3mfM1+8jLUesgUVCVSY=";
-            };
-            buildInputs = with pkgs; [
-              flex
-              bison
-              bash  # Needed by patchShebangs
-              bc
-              elfutils
-            ];
-            postPatch = ''
-              patchShebangs scripts/config
-            '';
-            configurePhase = builtins.readFile ./scripts/linux-configure.sh;
-            buildPhase = builtins.readFile ./scripts/linux-build.sh;
-            installPhase = builtins.readFile ./scripts/linux-install.sh;
-          };
+          linux = kernelStuff.package;
         });
 
       devShells = eachSystem (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          kernelStuff = import ./kernel { inherit pkgs self system; };
         in {
           go = import ./go.nix { inherit pkgs; };
           cancer = import ./cancer.nix { inherit pkgs; };
-          kernel = import ./kernel {
-            inherit pkgs;
-            inherit self system;
-          };
+          kernel = kernelStuff.devShell;
         });
     };
 }
--- kernel/default.nix
+++ kernel/default.nix
@@ -1,18 +1,41 @@
 { pkgs, self, system }:
 let
   name = ''\033[33mkernel-dev\033[39m'';
-  linux = self.packages.${system}.linux;
-in pkgs.mkShell {
-  buildInputs = with pkgs; [ linux ];
-  shellHook = ''
-    if [[ -n "$NIX_DEVELOP_STACK" ]]; then
-      export NIX_DEVELOP_STACK="$NIX_DEVELOP_STACK|${name}"
-    else
-      export NIX_DEVELOP_STACK="${name}"
-    fi
-    export PS1="\n\033[1m[$NIX_DEVELOP_STACK]\033[0m $PS1"
+  linux = pkgs.stdenv.mkDerivation {
+    pname = "linux";
+    version = "6.12.49";
+    src = pkgs.fetchurl {
+      url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.12.49.tar.xz";
+      hash = "sha256-I0Yh4UbazOIkEElVXVUOT3pr3mfM1+8jLUesgUVCVSY=";
+    };
+    buildInputs = with pkgs; [
+      flex
+      bison
+      bash  # Needed by patchShebangs
+      bc
+      elfutils
+    ];
+    postPatch = ''
+      patchShebangs scripts/config
+    '';
+    configurePhase = builtins.readFile ./configure.sh;
+    buildPhase = builtins.readFile ./build.sh;
+    installPhase = builtins.readFile ./install.sh;
+  };
+in {
+  package = linux;
+  devShell = pkgs.mkShell {
+    buildInputs = with pkgs; [ linux ];
+    shellHook = ''
+      if [[ -n "$NIX_DEVELOP_STACK" ]]; then
+        export NIX_DEVELOP_STACK="$NIX_DEVELOP_STACK|${name}"
+      else
+        export NIX_DEVELOP_STACK="${name}"
+      fi
+      export PS1="\n\033[1m[$NIX_DEVELOP_STACK]\033[0m $PS1"
 
-    export KERNEL_SRC=${linux}/src
-    export KERNEL_BUILD=${linux}/build
-  '';
+      export KERNEL_SRC=${linux}/src
+      export KERNEL_BUILD=${linux}/build
+    '';
+  };
 }
