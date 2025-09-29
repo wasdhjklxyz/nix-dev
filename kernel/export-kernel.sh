b328b87a5c70c436c52bf9c5927b07b4e3aaa41c
uiop <uiop@wasdhjkl.xyz>
2025-09-29 13:19:10 -0400

    fix(kernel): dont export if already exists

--- kernel/export-kernel.sh
+++ kernel/export-kernel.sh
@@ -1,12 +1,10 @@
 CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/nix-kernel-dev"
 export KDIR="$CACHE_DIR/$(basename "$LINUX_SRC")"
 
-if [[ -d "$KDIR" ]]; then
-  rm -rf "$KDIR"
+if [[ ! -d "$KDIR" ]]; then
+  mkdir -p "$KDIR"
+  cp -r "$LINUX_SRC" "$CACHE_DIR"
+  chmod -R +w "$KDIR"
+  find "$KDIR/scripts" -type f -exec chmod +x {} \; 2>/dev/null || true
+  find "$KDIR/tools" -type f -exec chmod +x {} \; 2>/dev/null || true
 fi
-mkdir -p "$KDIR"
-
-cp -r "$LINUX_SRC" "$CACHE_DIR"
-chmod -R +w "$KDIR"
-find "$KDIR/scripts" -type f -exec chmod +x {} \; 2>/dev/null || true
-find "$KDIR/tools" -type f -exec chmod +x {} \; 2>/dev/null || true
