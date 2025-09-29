273eba36a28a87c99ab5a824448610e8a1bbfb36
uiop <uiop@wasdhjkl.xyz>
2025-09-29 14:04:56 -0400

    fix(kernel): update paths in start-qemu

--- kernel/start-qemu.sh
+++ kernel/start-qemu.sh
@@ -1,14 +1,14 @@
 start-qemu() {
   qemu-system-x86_64 \
-    -kernel $KERNEL_BUILD/arch/x86_64/boot/bzImage \
-    -initrd $INITRAMFS_FINAL \
+    -kernel $KDIR/arch/x86_64/boot/bzImage \
+    -initrd $INITRAMFS \
     -nographic \
     -append "console=ttyS0 panic=1" \
 		-no-reboot \
 		-enable-kvm 2>/dev/null || \
   qemu-system-x86_64 \
-		-kernel $(BUILD_DIR)/bzImage \
-		-initrd $(BUILD_DIR)/initramfs.cpio \
+    -kernel $KDIR/arch/x86_64/boot/bzImage \
+    -initrd $INITRAMFS \
 		-nographic \
 		-no-reboot \
 		-append "console=ttyS0 panic=1"
