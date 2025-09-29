4ea4a4c621d1daa64e8a98efe818d2615d878cee
uiop <uiop@wasdhjkl.xyz>
2025-09-29 12:20:44 -0400

    fix(kernel): change export dir

--- kernel/export-kernel.sh
+++ kernel/export-kernel.sh
@@ -1,12 +1,12 @@
-PROJECT_HASH=$(echo -n "$PWD" | sha256sum | cut -c1-8)
-export KDIR="${XDG_CACHE_HOME:-$HOME/.cache}/nix-kernel-dev/$PROJECT_HASH"
+CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/nix-kernel-dev"
+export KDIR="$CACHE_DIR/$(basename "$LINUX_SRC")"
 
 if [[ -d "$KDIR" ]]; then
   rm -rf "$KDIR"
 fi
 mkdir -p "$KDIR"
 
-cp -r "$LINUX_SRC" "$KDIR"
+cp -r "$LINUX_SRC/*" "$KDIR"
 chmod -R +w "$KDIR"
-find "$KERNEL_SRC/scripts" -type f -exec chmod +x {} \; 2>/dev/null || true
-find "$KERNEL_SRC/tools" -type f -exec chmod +x {} \; 2>/dev/null || true
+find "$KDIR/scripts" -type f -exec chmod +x {} \; 2>/dev/null || true
+find "$KDIR/tools" -type f -exec chmod +x {} \; 2>/dev/null || true
