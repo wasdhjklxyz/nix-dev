db64e50c631bf45d37682279057fdfe6e6baf14f
uiop <uiop@wasdhjkl.xyz>
2025-09-29 14:01:39 -0400

    fix(initramfs): add write perms

--- kernel/export-initramfs.sh
+++ kernel/export-initramfs.sh
@@ -2,4 +2,5 @@ export INITRAMFS="$CACHE_DIR/$(basename "$INITRAMFS_SRC")/initramfs.cpio"
 if [[ ! -f "$INITRAMFS" ]]; then
   mkdir -p "$(dirname "$INITRAMFS")"
   cp "$INITRAMFS_SRC/initramfs.cpio" "$INITRAMFS"
+  chmod +w "$INITRAMFS"
 fi
