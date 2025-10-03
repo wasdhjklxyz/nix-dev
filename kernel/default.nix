00497dfb9e4465093a482f2898a2d5d2c4cfd200
uiop <uiop@wasdhjkl.xyz>
2025-10-03 11:00:37 -0400

    chore(kernel): nix dev stack export

--- kernel/default.nix
+++ kernel/default.nix
@@ -43,14 +43,8 @@ in {
   devShell = pkgs.mkShell {
     buildInputs = with pkgs; [ linux busybox ];
     shellHook = ''
-      if [[ -n "$NIX_DEVELOP_STACK" ]]; then
-        export NIX_DEVELOP_STACK="$NIX_DEVELOP_STACK|${name}"
-      else
-        export NIX_DEVELOP_STACK="${name}"
-      fi
-      export PS1="\n\033[1m[$NIX_DEVELOP_STACK]\033[0m $PS1"
-
-      CACHE_DIR="''${XDG_CACHE_HOME:-$HOME/.cache}/nix-kernel-dev"
+      NAME="kernel"
+      ${builtins.readFile ../nix-develop-stack.sh}
 
       LINUX_SRC=${linux}
       ${builtins.readFile ./export-kernel.sh}
