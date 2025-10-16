02879c8f5233b52082f49c174275d725ccbfa6cc
uiop <uiop@wasdhjkl.xyz>
2025-10-16 18:00:37 -0400

    feat(go): add delve

--- go.nix
+++ go.nix
@@ -1,9 +1,10 @@
 { pkgs }: pkgs.mkShell {
-  buildInputs = with pkgs; [ go gopls ];
+  buildInputs = with pkgs; [ go gopls delve ];
   shellHook = ''
     NAME="go"
     ${builtins.readFile ./nix-develop-stack.sh}
     go version
     gopls version
+    dlv version
   '';
 }
