0c0804314edd56d94fe84981223ae26a2917abd1
uiop <uiop@wasdhjkl.xyz>
2025-11-09 22:18:50 -0500

    feat: add python LSP pkgs

--- python.nix
+++ python.nix
@@ -1,10 +1,12 @@
 { pkgs }: pkgs.mkShell {
-  buildInputs = with pkgs; [ python314 uv ];
+  buildInputs = with pkgs; [ python314 uv basedpyright ruff ];
   shellHook = ''
     NAME="python"
     ${builtins.readFile ./nix-develop-stack.sh}
     export UV_PROJECT_ENVIRONMENT=".venv"
     python --version
     uv --version
+    basedpyright --version
+    ruff --version
   '';
 }
