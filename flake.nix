e0c6a7330ab5f747dace8763d464e2df0c04798b
uiop <uiop@wasdhjkl.xyz>
2025-11-13 17:22:08 -0500

    fix: merge conflict error

--- flake.nix
+++ flake.nix
@@ -10,21 +10,6 @@
     let
       eachSystem = nixpkgs.lib.genAttrs (import systems);
     in {
-      packages = eachSystem (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          kernelStuff = import ./kernel { inherit pkgs self system; };
-        in {
-          linux = pkgs.stdenv.mkDerivation {
-            pname = "linux";
-            version = "6.12.49";
-            src = pkgs.fetchurl {
-              url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.12.49.tar.xz";
-              hash = "sha256-I0Yh4UbazOIkEElVXVUOT3pr3mfM1+8jLUesgUVCVSY=";
-            };
-          };
-        });
-
       devShells = eachSystem (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
@@ -32,7 +17,6 @@
         in {
           go = import ./go.nix { inherit pkgs; };
           cancer = import ./cancer.nix { inherit pkgs; };
-          kernel = kernelStuff.devShell;
           sqlite = import ./sqlite.nix { inherit pkgs; };
           cmake = import ./cmake.nix { inherit pkgs; };
           curl = import ./curl.nix { inherit pkgs; };
