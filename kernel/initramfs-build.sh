c7b1242204841a5ef3e455ba7b59012efac9c463
uiop <uiop@wasdhjkl.xyz>
2025-12-02 19:24:20 -0500

    feat(kernel): add multi VM support w/ bridge
    - Create separate TAP devices per VM role (client/server/peer)
    - Bridge TAP devices for inter-VM communication
    - Pass role parameters via kernel cmdline for VM-specific config
    - Assign unique IPs, MACs, and colored prompts per role
    - Update qemu-tty to support connecting to specific VM sockets
    
    NOTE: This is for development of https://github.com/wasdhjklxyz/rtrd

--- kernel/initramfs-build.sh
+++ kernel/initramfs-build.sh
@@ -11,6 +11,15 @@ mount -t devtmpfs none /dev
 mount -t proc none /proc
 mount -t sysfs none /sys
 
+for param in $(cat /proc/cmdline); do
+  case $param in
+    role=*) ROLE=${param#*=} ;;
+    vm_ip=*) VM_IP=${param#*=} ;;
+    ps1_color=*) PS1_COLOR=${param#*=} ;;
+    ps1_name=*) PS1_NAME=${param#*=} ;;
+  esac
+done
+
 if grep -q 9p /proc/filesystems 2>/dev/null; then
   mkdir -p /mnt/host
   mount -t 9p -o trans=virtio,version=9p2000.L hostshare /mnt/host
@@ -20,8 +29,10 @@ ip link set lo up
 ip addr add 127.0.0.1/8 dev lo
 
 ip link set eth0 up
-ip addr add 10.0.3.2/24 dev eth0
+ip addr add ${VM_IP:-10.0.3.2}/24 dev eth0
 ip route add default via 10.0.3.1
+
+export PS1="\[\e[1;${PS1_COLOR:-39}m\][${PS1_NAME:-root}@${VM_IP:-10.0.3.2}:\w]\[\e[0m\] # "
 
 setsid /bin/sh </dev/ttyS1 >/dev/ttyS1 2>&1 &
 exec /bin/sh
--- kernel/start-qemu.sh
+++ kernel/start-qemu.sh
@@ -1,40 +1,87 @@
 start-qemu() {
+  local role=${1:-client}
+  local vm_ip mac_suffix ps1_color ps1_name tap_dev sock_path
+
   if ! grep -q CONFIG_9P_FS=y $KDIR/.config 2>/dev/null; then
     echo "Warning: Kernel may not have 9P support (CONFIG_9P_FS)" >&2
   fi
-  if ! ip link show tap0 &>/dev/null; then
-    echo "Creating tap0..."
-    sudo ip tuntap add dev tap0 mode tap user $USER
-    sudo ip link set tap0 up
-    sudo ip addr add 10.0.3.1/24 dev tap0
+
+  case $role in
+    client)
+      vm_ip="10.0.3.2"
+      mac_suffix="02"
+      ps1_color="34" # blue
+      ps1_name="client"
+      tap_dev="tap_client"
+      sock_path="/tmp/qemu-client.sock"
+      ;;
+    server)
+      vm_ip="10.0.3.3"
+      mac_suffix="03"
+      ps1_color="32" # green
+      ps1_name="server"
+      tap_dev="tap_server"
+      sock_path="/tmp/qemu-server.sock"
+      ;;
+    peer)
+      vm_ip="10.0.3.4"
+      mac_suffix="04"
+      ps1_color="31" # red
+      ps1_name="peer"
+      tap_dev="tap_peer"
+      sock_path="/tmp/qemu-peer.sock"
+      ;;
+    *)
+      echo "Usage: $0 {client|server|peer}" >&2
+      return 1
+      ;;
+  esac
+
+  if ! ip link show $tap_dev &>/dev/null; then
+    echo "Creating $tap_dev..."
+    sudo ip tuntap add dev $tap_dev mode tap user $USER
+    sudo ip link set $tap_dev up
   fi
+
+  if ! ip link show br0 &>/dev/null; then
+    echo "Creating bridge br0..."
+    sudo ip link add br0 type bridge
+    sudo ip link set br0 up
+    sudo ip addr add 10.0.3.1/24 dev br0
+  fi
+
+  if ! bridge link show | grep -q $tap_dev; then
+    sudo ip link set $tap_dev master br0
+  fi
+
   qemu-system-x86_64 \
     -kernel $KDIR/arch/x86_64/boot/bzImage \
     -initrd $INITRAMFS \
     -nographic \
-    -append "console=ttyS0 panic=1" \
+    -append "console=ttyS0 panic=1 role=$role vm_ip=$vm_ip ps1_color=$ps1_color ps1_name=$ps1_name" \
 		-no-reboot \
     -virtfs local,path=$(pwd),mount_tag=hostshare,security_model=none \
     -serial mon:stdio \
-    -serial unix:/tmp/qemu-serial.sock,server,nowait \
-    -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
-    -device virtio-net-pci,netdev=net0 \
+    -serial unix:$sock_path,server,nowait \
+    -netdev tap,id=net0,ifname=$tap_dev,script=no,downscript=no \
+    -device virtio-net-pci,netdev=net0,mac=52:54:00:00:00:${mac_suffix} \
 		-enable-kvm 2>/dev/null || \
   qemu-system-x86_64 \
     -kernel $KDIR/arch/x86_64/boot/bzImage \
     -initrd $INITRAMFS \
 		-nographic \
 		-no-reboot \
-		-append "console=ttyS0 panic=1" \
+    -append "console=ttyS0 panic=1 role=$role vm_ip=$vm_ip ps1_color=$ps1_color ps1_name=$ps1_name" \
     -serial mon:stdio \
-    -serial unix:/tmp/qemu-serial.sock,server,nowait \
+    -serial unix:$sock_path,server,nowait \
     -virtfs local,path=$(pwd),mount_tag=hostshare,security_model=none \
-    -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
-    -device virtio-net-pci,netdev=net0
+    -netdev tap,id=net0,ifname=$tap_dev,script=no,downscript=no \
+    -device virtio-net-pci,netdev=net0,mac=52:54:00:00:00:${mac_suffix}
 }
 
 qemu-tty() {
-  local sock="/tmp/qemu-serial.sock"
+  local role=${1:-client}
+  local sock="/tmp/qemu-${role}.sock"
   [[ ! -S "$sock" ]] && { echo "No QEMU socket at $sock" >&2; return 1; }
   socat unix-connect:"$sock" -,raw,echo=0
 }
