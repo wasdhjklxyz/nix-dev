0adfa1d9cf715bc4293125a6e42d6f90557e656c
uiop <uiop@wasdhjkl.xyz>
2025-12-03 01:37:30 -0500

    fix(kernel): vmlinux gdb symlink

--- kernel/export-kernel.sh
+++ kernel/export-kernel.sh
@@ -6,6 +6,7 @@ if [[ ! -d "$KDIR" ]]; then
   chmod -R +w "$KDIR"
   find "$KDIR/scripts" -type f -exec chmod +x {} \; 2>/dev/null || true
   find "$KDIR/tools" -type f -exec chmod +x {} \; 2>/dev/null || true
+  ln -sf "$KDIR/scripts/gdb/vmlinux-gdb.py" "$KDIR/vmlinux-gdb.py"
 fi
 
 export KERNEL_VERSION=${LINUX_SRC#*-linux-}
