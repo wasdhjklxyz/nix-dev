55eba8ae7272f6134e976b2f35321b96573e29bf
uiop <uiop@wasdhjkl.xyz>
2025-12-03 01:36:47 -0500

    fix(kernel): debug configuration

--- kernel/configure.sh
+++ kernel/configure.sh
@@ -49,6 +49,13 @@ make allnoconfig
   --enable CONFIG_IP_MULTIPLE_TABLES \
   --enable CONFIG_NET_FOU \
   --enable CONFIG_VETH \
+  --enable CONFIG_DEBUG_KERNEL \
   --enable CONFIG_DEBUG_INFO \
-  --enable CONFIG_GDB_SCRIPTS
+  --enable CONFIG_DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT \
+  --disable CONFIG_DEBUG_INFO_REDUCED \
+  --disable CONFIG_DEBUG_INFO_COMPRESSED \
+  --disable CONFIG_DEBUG_INFO_SPLIT \
+  --enable CONFIG_GDB_SCRIPTS \
+  --disable CONFIG_STRICT_KERNEL_RWX \
+  --disable CONFIG_STRICT_MODULE_RWX
 make olddefconfig
