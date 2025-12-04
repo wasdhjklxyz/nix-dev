ffd896103ecf0c52406c82fb8598612dbb970d48
uiop <uiop@wasdhjkl.xyz>
2025-12-03 22:22:35 -0500

    feat(cancer): add yarn

--- cancer.nix
+++ cancer.nix
@@ -1,8 +1,9 @@
 { pkgs }: pkgs.mkShell {
-  buildInputs = with pkgs; [ nodejs ];
+  buildInputs = with pkgs; [ nodejs yarn ];
   shellHook = ''
     NAME="cancer"
     ${builtins.readFile ./nix-develop-stack.sh}
     echo "npm version $(npm --version)"
+    echo "yarn version $(yarn --version)"
   '';
 }
