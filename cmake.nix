96c220d33d37b0203076b3e0af3db9fbdf031cad
uiop <uiop@wasdhjkl.xyz>
2026-02-21 05:10:57 -0500

    feat(cmake): add pkg-config

--- cmake.nix
+++ cmake.nix
@@ -1,8 +1,9 @@
 { pkgs }: pkgs.mkShell {
-  buildInputs = with pkgs; [ cmake ];
+  buildInputs = with pkgs; [ cmake pkg-config ];
   shellHook = ''
     NAME="cmake"
     ${builtins.readFile ./nix-develop-stack.sh}
     cmake --version | head -1
+    echo "pkg-config version $(pkg-config --version)"
   '';
 }
