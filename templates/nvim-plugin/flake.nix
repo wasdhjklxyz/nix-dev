87b5b5129a5b680fed4fac2c5073fa8295046c27
uiop <uiop@wasdhjkl.xyz>
2026-07-23 10:12:38 -0400

    nvim-plugin: generate .luarc.json

--- templates/nvim-plugin/flake.nix
+++ templates/nvim-plugin/flake.nix
@@ -18,6 +18,7 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          neovim-dev-dir = ".nvim-dev";
           neovim-dev = pkgs.writeShellApplication {
             name = "nvim-dev";
             runtimeInputs = with pkgs; [
@@ -31,7 +32,7 @@
             text = ''
               root="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
               export NVIM_DEV_PLUGIN="''${NVIM_DEV_PLUGIN:-$root}"
-              dev="$root/.nvim-dev"
+              dev="$root/${neovim-dev-dir}"
               export XDG_CONFIG_HOME="$dev/config"
               export XDG_DATA_HOME="$dev/data"
               export XDG_STATE_HOME="$dev/state"
@@ -50,7 +51,20 @@
             ];
             inputsFrom = [ ];
             env = { };
-            shellHook = "";
+            shellHook = ''
+              cat > .luarc.json <<'EOF'
+              {
+                "runtime.version": "LuaJIT",
+                "workspace.library": [
+                  "${pkgs.neovim-unwrapped}/share/nvim/runtime/lua",
+                  "''${3rd}/luv/library",
+                  "${neovim-dev-dir}/data/nvim/lazy/lazy.nvim/lua"
+                ],
+                "workspace.checkThirdParty": false,
+                "workspace.ignoreDir": ["${neovim-dev-dir}"]
+              }
+              EOF
+            '';
           };
         }
       );
